954b76d0ecfcbf6da9b8c70f9385023dce992acd
Jair4x <Jair4x@gmail.com>
2026-07-22 22:02:19 -0300

    chore: initial Tauri + React + Tailwind scaffold

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# mindsparks
+# MindSparks
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="w-screen h-screen bg-neutral-950 text-neutral-100 flex items-center justify-center">
+      <p className="text-2xl font-semibold">MindSparks</p>
+    </main>
+  );
+}
+
+export default App;
